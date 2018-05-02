91d64d49ba5a5259ad0e5f62ff82667ab0576303
SzilardZ <szilard.zr@gmail.com>
2018-05-02 15:49:11 +0200

    add new question ready

--- data_manager.py
+++ data_manager.py
@@ -4,9 +4,16 @@ import util
 @util.connection_handler
 def get_question_and_title(cursor):
     cursor.execute("""
-                    SELECT id, title, message FROM question
+                    SELECT * FROM question;
                     """)
     questions = cursor.fetchall()
     return questions
+
+
+@util.connection_handler
+def add_question(cursor, new_title, new_message):
+    cursor.execute("""
+                    INSERT INTO question (title, message) VALUES (%(title)s, %(message)s);
+                    """, {'title': new_title, 'message': new_message})
 
 
