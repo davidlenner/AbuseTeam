f2454d0e6a3b143e41a9acffe3d3e7694115239b
davidlenner <david910128@gmail.com>
2018-05-16 17:02:33 +0200

    we are trying to solve some merge conflitcts

--- data_manager.py
+++ data_manager.py
@@ -2,12 +2,53 @@ import util
 
 
 @util.connection_handler
-def get_question_and_title(cursor):
+def get_questions(cursor):
     cursor.execute("""
                     SELECT * FROM question;
                     """)
     questions = cursor.fetchall()
+
     return questions
+
+
+@util.connection_handler
+def get_question_by_id(cursor, id):
+    cursor.execute("""
+                    SELECT * FROM question
+                    WHERE id = %(id)s;
+                    """,
+                   {'id': id})
+    question = cursor.fetchall()
+
+    return question
+
+
+@util.connection_handler
+def add_question(cursor, new_title, new_message, time, user_id):
+    cursor.execute("""
+                    INSERT INTO question (title, message, submission_time, user_id)
+                    VALUES (%(title)s, %(message)s, %(submission_time)s, %(user_id)s);
+                    """,
+                   {'title': new_title, 'message': new_message, 'submission_time': time, 'user_id': user_id})
+
+
+@util.connection_handler
+def edit_question(cursor, edited_title, edited_message, id):
+    cursor.execute("""
+                    UPDATE question
+                    SET title=%(title)s, message=%(message)s
+                    WHERE id = %(id)s;
+                    """,
+                   {'title': edited_title, 'message': edited_message, 'id': id})
+
+
+@util.connection_handler
+def delete_question(cursor, id):
+    cursor.execute("""
+                    DELETE FROM question
+                    WHERE id = %(id)s;
+                    """,
+                   {'id': id})
 
 
 @util.connection_handler
@@ -22,37 +63,18 @@ def get_answers(cursor, id):
 
 
 @util.connection_handler
-def question_by_id(cursor, id):
+def add_answer(cursor, new_id, new_message, time, user_id):
     cursor.execute("""
-                    SELECT * FROM question
-                    WHERE id = %(id)s;
+                    INSERT INTO answer (question_id, message, submission_time, user_id)
+                    VALUES (%(question_id)s, %(message)s, %(submission_time)s, %(user_id)s);
                     """,
-                   {'id': id})
-    question = cursor.fetchall()
-    return question
-
-
-@util.connection_handler
-def add_question(cursor, new_title, new_message, time):
-    cursor.execute("""
-                    INSERT INTO question (title, message, submission_time)
-                    VALUES (%(title)s, %(message)s, %(submission_time)s);
-                    """,
-                   {'title': new_title, 'message': new_message, 'submission_time': time})
-
-
-@util.connection_handler
-def add_answer(cursor, new_id, new_message,time):
-    cursor.execute("""
-                    INSERT INTO answer (question_id, message, submission_time)
-                    VALUES (%(question_id)s, %(message)s, %(submission_time)s);
-                    """,
-                   {'question_id': new_id, 'message': new_message, 'submission_time': time})
+                   {'question_id': new_id, 'message': new_message, 'submission_time': time, 'user_id': user_id})
 
 
 @util.connection_handler
 def get_answer_by_id(cursor, id):
-    cursor.execute("""SELECT * FROM answer
+    cursor.execute("""
+                    SELECT * FROM answer
                     WHERE id = %(id)s;
                     """,
                    {'id': id})
@@ -61,18 +83,72 @@ def get_answer_by_id(cursor, id):
 
 
 @util.connection_handler
-def update_answer(cursor, id, message):
-    cursor.execute("""UPDATE answer
+def edit_answer(cursor, id, message):
+    cursor.execute("""
+                    UPDATE answer
                     SET message = %(message)s 
                     WHERE id = %(id)s;""",
                    {'id': id, 'message': message})
 
 
 @util.connection_handler
-def edit_question(cursor, edited_title, edited_message, id):
+def delete_answer(cursor, id):
     cursor.execute("""
-                    UPDATE question
-                    SET title=%(title)s, message=%(message)s
-                    WHERE id = %(id)s;
+                    DELETE FROM answer
+                    WHERE id = %(id)s
                     """,
-                   {'title': edited_title, 'message': edited_message, 'id': id})
+                   {'id': id})
+
+
+@util.connection_handler
+def delete_answer_by_question_id(cursor, question_id):
+    cursor.execute("""
+                        DELETE FROM answer
+                        WHERE question_id = %(question_id)s
+                        """,
+                        {'question_id': question_id})
+
+
+@util.connection_handler
+def get_password(cursor, typed_user_name):
+    cursor.execute("""
+                    SELECT password
+                    FROM users
+                    WHERE user_name =%(user_name)s;
+                    """,
+                   {'user_name': typed_user_name})
+    database_password = cursor.fetchall()
+
+    return database_password
+
+
+@util.connection_handler
+def check_user(cursor):
+    cursor.execute("""
+                    SELECT *
+                    FROM users;
+                    """,)
+    users = cursor.fetchall()
+
+    return users
+
+
+@util.connection_handler
+def get_user_id(cursor, user_name):
+    cursor.execute("""
+                    SELECT id
+                    FROM users
+                    WHERE user_name=%(user_name)s;
+                    """,
+                   {'user_name': user_name})
+    user_id = cursor.fetchall()
+
+    return user_id
+
+
+@util.connection_handler
+def registration(cursor, username, password, time):
+    cursor.execute("""INSERT INTO users (user_name,password,registration_time) VALUES (%(user_name)s,%(password)s,%(registration_time)s);
+                    """, {'user_name': username, 'password': password, 'registration_time': time})
+
+
--- server.py
+++ server.py
@@ -1,61 +1,124 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import data_manager
 from datetime import datetime
-
+import pwhashing
 
 app = Flask(__name__)
+app.secret_key = 'AskMateSecretKey'
 
 
 @app.route("/")
-@app.route("/list")
-def list():
-    questions = data_manager.get_question_and_title()
-    return render_template('list.html', questions=reversed(questions))
+@app.route("/list", methods=['GET', 'POST'])
+def route_list():
+    questions = data_manager.get_questions()
+    if request.method == 'GET':
+        return render_template('list.html', questions=reversed(questions))
+
+    elif request.method == 'POST':
+        user_input_username = request.form['user_input_username']
+        user_input_password = request.form['user_input_password']
+        user_database_password = data_manager.get_password(user_input_username)
+        for dict in user_database_password:
+            for key, value in dict.items():
+                if pwhashing.verify_password(user_input_password, value):
+                    session['user_id'] = data_manager.get_user_id(user_input_username)
+                    is_logged_in = "true"
+                    message = "Hi " + user_input_username + "!"
+                    return render_template('list.html', questions=reversed(questions), is_logged_in=is_logged_in, message=message)
+
+                else:
+                    message = "Invalid username or password!"
+                    is_logged_in = "false"
+                    return render_template('list.html', questions=reversed(questions), is_logged_in=is_logged_in, message=message)
 
 
-@app.route("/add-question", methods=["GET", "POST"])
+@app.route('/add-question', methods=['GET', 'POST'])
 def add_question():
-    if request.method == "GET":
+    if request.method == 'GET':
         return render_template('add_question.html')
     dt = datetime.now()
-    data_manager.add_question(request.form['title'], request.form['message'], dt)
-    return redirect("/")
+    for dict in session['user_id']:
+        for key, value in dict.items():
+            data_manager.add_question(request.form['title'], request.form['message'], dt, value)
+            return redirect('/')
 
 
-@app.route("/question/<question_id>", methods=['GET'])
-def question(question_id):
+@app.route('/question/<question_id>', methods=['GET'])
+def question_by_id(question_id):
     answer = data_manager.get_answers(question_id)
-    question = data_manager.question_by_id(question_id)
+    question = data_manager.get_question_by_id(question_id)
     return render_template('question_id.html', question=question, answer=answer, question_id=question_id)
 
 
-@app.route("/question/<question_id>/add-answer", methods=['GET', 'POST'])
-def add_answer(question_id):
-    question = data_manager.question_by_id(question_id)
-    if request.method == 'GET':
-        return render_template('add_answer.html', question=question)
-    message = request.form.get('message', '')
-    dt = datetime.now()
-    data_manager.add_answer(question_id, message, dt)
-    return redirect('/')
-
-
-@app.route("/answer/<answer_id>/edit_answer", methods=['GET', 'POST'])
-def edit_answer(answer_id):
-    if request.method == 'GET':
-        answer = data_manager.get_answer_by_id(answer_id)
-        return render_template('edit_answer.html', answer=answer)
-    data_manager.update_answer(answer_id, request.form['message'])
-    return redirect('/')
-
-
-@app.route("/question/<question_id>/edit_question", methods=['GET', 'POST'])
+@app.route('/question/<question_id>/edit-question', methods=['GET', 'POST'])
 def edit_question(question_id):
-    if request.method == "GET":
-        question = data_manager.question_by_id(question_id)
+    if request.method == 'GET':
+        question = data_manager.get_question_by_id(question_id)
         return render_template('edit_question.html', question=question, question_id=question_id)
     data_manager.edit_question(request.form['title'], request.form['message'], question_id)
     return redirect('/')
+
+
+@app.route('/question/<question_id>/delete-question')
+def delete_question(question_id):
+    data_manager.delete_answer_by_question_id(question_id)
+    data_manager.delete_question(question_id)
+    return redirect('/')
+
+
+@app.route('/question/<question_id>/add-answer', methods=['GET', 'POST'])
+def add_answer(question_id):
+    question = data_manager.get_question_by_id(question_id)
+    if request.method == 'GET':
+        return render_template('add_answer.html', question=question)
+
+    message = request.form.get('message', '')
+    dt = datetime.now()
+    for dict in session['user_id']:
+        for key, value in dict.items():
+            data_manager.add_answer(question_id, message, dt, value)
+            return redirect('/')
+
+
+@app.route('/question/<question_id>/answer/<answer_id>/edit-answer', methods=['GET', 'POST'])
+def edit_answer(question_id, answer_id):
+    data_manager.get_question_by_id(question_id)
+    if request.method == 'GET':
+        answer = data_manager.get_answer_by_id(answer_id)
+        return render_template('edit_answer.html', answer=answer)
+    data_manager.edit_answer(answer_id, request.form['message'])
+    return redirect('/')
+
+
+@app.route('/question/<question_id>/answer/<answer_id>/delete-answer')
+def delete_answer(question_id, answer_id):
+    data_manager.get_question_by_id(question_id)
+    data_manager.delete_answer(answer_id)
+    return redirect('/')
+
+
+@app.route("/registration", methods=['GET', 'POST'])
+def registration():
+    if request.method == 'POST':
+        usernames = data_manager.check_usernames()
+        username = request.form.get('username', '')
+        for dict in usernames:
+            for key, values in dict.items():
+                if values != username:
+                    time = datetime.now()
+                    hashedpw = pwhashing.hash_password(request.form.get('password', ''))
+                    data_manager.registration(username, hashedpw, time)
+                    return redirect('/')
+                elif values == username:
+                    used_name = 'False'
+                    return render_template('registration.html', used_name=used_name)
+    return render_template('registration.html')
+
+
+@app.route("/logged_out", methods=['POST'])
+def logged_out():
+    session.clear()
+    return redirect('/list')
 
 
 if __name__ == '__main__':
